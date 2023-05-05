b8ef6397630427bee0e79e30a3227662f2f1bced
Tom <todepussay@stpbb.org>
2023-05-05 11:29:23 +0200

    Index2

---
+++ header.php
@@ -0,0 +1,18 @@
+<header>
+    <div class="left">
+        <a href="index.php">
+            <img src="images/ACCUEIL.jpg" alt="Accueil">
+            <h1>Société Lafleur</h1>
+        </a>
+    </div>
+    <div class="right">
+        <nav>
+            <ul>
+                <li><a href="categorie.php?id=bul">Bulbes</a></li>
+                <li><a href="categorie.php?id=mas">Plantes à massif</a></li>
+                <li><a href="categorie.php?id=ros">Rosier</a></li>
+                <li><a href="connexion.php">Connexion</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
